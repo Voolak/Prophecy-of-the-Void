403a36f3aac444e64e5c81c0db9ca918e529cbf3
Voolak <abarzana.perso@gmail.com>
2024-05-11 22:09:10 +0200

    Player Smooth Rotation

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,6 +1,7 @@
 extends RigidBody2D
 
 @export var engine_power = 800
+@export var rotation_speed = 5.0
 
 var thrust = Vector2.ZERO
 
@@ -18,10 +19,25 @@ func get_input():
 func _physics_process(_delta):
 	get_input()
 	constant_force = thrust.rotated(rotation)
-	
+	#add_constant_force(thrust.rotated(rotation))
+
 	var cursor_pos = get_global_mouse_position()
 	var direction = (cursor_pos - global_position).normalized()
 
-	var angle = direction.angle()
+	var target_angle = direction.angle()
 
-	$Sprite2D.rotation = angle
+	var current_angle = $Sprite2D.rotation
+	var new_angle = lerp_angle(current_angle, target_angle, rotation_speed * _delta)
+	$Sprite2D.rotation = new_angle
+
+func lerp_angle(a, b, t):
+	var angle_diff = (b - a + PI) % (2 * PI) - PI
+	return a + angle_diff * clamp(t, 0, 1)
+
+func clamp(value, min, max):
+	if value < min:
+		return min
+	elif value > max:
+		return max
+	else:
+		return value
