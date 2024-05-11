84515621ae2d61a1e254ee63811214f48e97a4e8
Voolak <abarzana.perso@gmail.com>
2024-05-11 21:51:44 +0200

    Player Look at cursor

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,22 +1,27 @@
 extends RigidBody2D
 
 @export var engine_power = 800
-@export var spin_power = 10000
 
 var thrust = Vector2.ZERO
-var rotation_dir = 0
 
 func get_input():
 	thrust = Vector2.ZERO
 	if Input.is_action_pressed("up"):
-		thrust.y -= engine_power  # Move up
+		thrust.y -= engine_power
 	if Input.is_action_pressed("down"):
-		thrust.y += engine_power  # Move down
+		thrust.y += engine_power
 	if Input.is_action_pressed("left"):
-		thrust.x -= engine_power  # Move left
+		thrust.x -= engine_power
 	if Input.is_action_pressed("right"):
-		thrust.x += engine_power  # Move right
+		thrust.x += engine_power
 
 func _physics_process(_delta):
 	get_input()
-	constant_force = thrust.rotated(rotation)  # Apply force in the direction of the ship
+	constant_force = thrust.rotated(rotation)
+	
+	var cursor_pos = get_global_mouse_position()
+	var direction = (cursor_pos - global_position).normalized()
+
+	var angle = direction.angle()
+
+	$Sprite2D.rotation = angle
