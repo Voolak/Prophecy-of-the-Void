2c321f5af12b69f3b1dc7c43d44ad39089f0abf9
Gozea <rbournet@gmail.com>
2024-05-12 11:25:20 +0200

    abstraction

---
+++ Scripts/KillZone.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+@onready var timer = $Timer
+@export var HIT_DAMAGE: int
+
+func _on_body_entered(body):
+	timer.start()
+	print("collision")
+
+
+func _on_timer_timeout():
+	get_tree().reload_current_scene()
