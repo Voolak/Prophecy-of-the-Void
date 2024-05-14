55705a1a4057f0828aee6561a18da649eda07949
Voolak <abarzana.perso@gmail.com>
2024-05-14 13:57:31 +0200

    Slime Movment

--- Scripts/Slimes.gd
+++ Scripts/Slimes.gd
@@ -1,19 +1,24 @@
 extends Node2D
 
+@export var speed = 200
 @export var hp = 1
 
-var velocity = Vector2.ZERO
+var velocity = Vector2.LEFT
 
 @onready var player = %Player
 @onready var screensize = get_viewport_rect().size
 @onready var animation_player = $AnimationPlayer
+@onready var scaling_timer = $ScalingTimer
 
 
 func _physics_process(delta):
+	velocity = velocity.normalized() * speed
 	global_transform = global_transform.translated(velocity * delta)
+	
 	position.x = wrapf(position.x, 0, screensize.x)
 	position.y = wrapf(position.y, 0, screensize.y)
-	rotation = velocity.angle()
+	
+	rotation += 0.5 * delta
 
 # on bullet enter
 func _on_hit_box_area_entered(area):
