8eb31d201c836b25eedce33df5733bb020fdef29
Voolak <abarzana.perso@gmail.com>
2024-05-14 17:58:24 +0200

    Slime scaling

--- Scripts/Slimes.gd
+++ Scripts/Slimes.gd
@@ -1,15 +1,17 @@
 extends Node2D
 
-@export var speed = 200
-@export var hp = 1
+@export var speed: float = 200
+@export var hp: int = 1
+@export var direction: Vector2 = Vector2.ZERO
 
-var velocity = Vector2.LEFT
-
+var velocity: Vector2 = Vector2.ZERO
 @onready var player = %Player
 @onready var screensize = get_viewport_rect().size
 @onready var animation_player = $AnimationPlayer
-@onready var scaling_timer = $ScalingTimer
-
+	
+func _ready():
+	animation_player.play("scaling")
+	velocity = direction.normalized()
 
 func _physics_process(delta):
 	velocity = velocity.normalized() * speed
@@ -24,9 +26,8 @@ func _physics_process(delta):
 func _on_hit_box_area_entered(area):
 	hp -= 1
 	if hp <= 0:
-		$".".animation_player.play("death")
+		animation_player.play("death")
 	area.queue_free()	#delete bullet
 
-
-func _on_scaling_timer_timeout():
-	pass # Replace with function body.
+func multiply():
+	pass
