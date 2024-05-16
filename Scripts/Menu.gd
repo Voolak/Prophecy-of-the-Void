97aaaa5cf78d9f68ad879a172344fdde2801379e
Voolak <abarzana.perso@gmail.com>
2024-05-16 17:51:55 +0200

    Level Paralax

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -1,10 +1,11 @@
 extends Control
+@onready var parallax_background = $ParallaxBackground
 
 func _ready():
 	get_viewport().size = DisplayServer.screen_get_size()
 
 func _process(delta):
-	$ParallaxBackground.scroll_offset=get_global_mouse_position()
+	parallax_background.scroll_offset=get_global_mouse_position()
 
 
 func _on_play_pressed():
